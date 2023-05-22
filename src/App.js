730e6e396d5269b7f23a081248b4ce9b379f6423
Katherine Granados <kathgmasis@gmail.com>
2023-05-22 11:44:39 -0600

    handling-events

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import TodoList from "./components/TodoList";
 import Profile from "./components/Profile";
 import PackingList from "./components/PackingList";
 import RenderList from "./components/RenderList";
+import Button from "./components/Button";
+import Toolbar from "./components/Toolbar";
 
 
 export default function Gallery() {
   return (
     <section>
+      <Button/>
+      <Toolbar/>
       <h1>Amazing scientists</h1>
       <Profile />
       <TodoList/>
