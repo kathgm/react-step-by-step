ee2684165f0546df573fccd15d808bb7b803ab6f
Katherine Granados <kathgmasis@gmail.com>
2023-05-22 15:06:51 -0600

    states

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import PackingList from "./components/PackingList";
 import RenderList from "./components/RenderList";
 import Button from "./components/Button";
 import Toolbar from "./components/Toolbar";
+import Counter from "./components/Counter";
+import Form from "./components/Form";
 
 
 export default function Gallery() {
   return (
     <section>
+      <Form/>
+      <Counter/>
       <Button/>
       <Toolbar/>
       <h1>Amazing scientists</h1>
